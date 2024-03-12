f8876069e60746d6db84b0dcb8b1bb1f2b5f6e60
ihtesham ul haq <ihteshamulhaq510@gmail.com>
2024-03-12 22:27:58 +0500

    added ui components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-'use client'
 import Header from '@/components/header'
+import Files from '@/components/Files'
 export default function Home() {
 	return (
 		<main>
 			<Header />
-			<div className='flex min-h-screen flex-col items-center  p-24'></div>
+			<div className='m-8 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-4'>
+				<Files />
+			</div>
 		</main>
 	)
 }
