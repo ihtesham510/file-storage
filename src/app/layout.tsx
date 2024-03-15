2cbc2ae47b9f41a505f1ebc1d4ef7550eae82ded
ihtesham ul haq <ihteshamulhaq510@gmail.com>
2024-03-15 14:29:40 +0500

    added toast

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import ConvexClientProvider from './ConvexClientProvider'
 import { ThemeProvider } from '@/components/theme-provider'
+import { Toaster } from '@/components/ui/toaster'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,7 +21,10 @@ export default function RootLayout({
 		<html lang='en'>
 			<body className={inter.className}>
 				<ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
-					<ConvexClientProvider>{children}</ConvexClientProvider>
+					<ConvexClientProvider>
+						<Toaster />
+						{children}
+					</ConvexClientProvider>
 				</ThemeProvider>
 			</body>
 		</html>
